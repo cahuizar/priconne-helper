daa1d525414e0f92ec92771444c5912007df21fa
Carlos Huizar <hire@carloshuizar.com>
2021-03-01 22:12:30 -0500

    Create footer

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
   <v-app>
     <p-nav />
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
     </v-main>
+    <p-footer />
   </v-app>
 </template>
 
 <script>
 import logo from '@/assets/logo.jpg';
+import Footer from '@/components/Layout/Footer.vue';
 import HelloWorld from './components/HelloWorld.vue';
 import Nav from './components/Layout/Nav.vue';
 
@@ -17,6 +19,7 @@ export default {
   components: {
     HelloWorld,
     PNav: Nav,
+    PFooter: Footer,
   },
   data() {
     return {
---
+++ src/components/Layout/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-footer
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      align="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="icon in icons"
+        :key="icon.icon"
+        :href="icon.to"
+        color="white"
+        icon
+        class="my-3 mx-2"
+      >
+        <v-icon size="26px">{{icon.icon}}</v-icon>
+      </v-btn>
+      <v-col
+        class="primary lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        Last updated: March 01, 2021
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      icons: [
+        {
+          to: 'https://buymecoffee.com',
+          icon: 'mdi-coffee',
+        },
+        {
+          to: 'https://github.com/cahuizar/priconne-helper',
+          icon: 'mdi-github',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
