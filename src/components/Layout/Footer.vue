e1fd6104bcebeff952d9c02f5e09becade54747d
Carlos Huizar <hire@carloshuizar.com>
2021-03-06 20:24:52 -0500

    Create footer

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-footer
-    color="primary lighten-1"
+    color="primary"
     padless
   >
     <v-row
@@ -8,18 +8,9 @@
       align="center"
       no-gutters
     >
-      <v-btn
-        v-for="icon in icons"
-        :key="icon.icon"
-        :href="icon.to"
-        color="white"
-        icon
-        class="my-3 mx-2"
-      >
-        <v-icon size="26px">{{icon.icon}}</v-icon>
-      </v-btn>
+      <p-nav-icons classes="my-3 mx-2"/>
       <v-col
-        class="primary lighten-2 py-4 text-center white--text"
+        class="primary lighten-1 py-4 text-center white--text"
         cols="12"
       >
         Last updated: March 01, 2021
@@ -29,21 +20,12 @@
 </template>
 
 <script>
+import NavIcons from '@/components/NavIcons/NavIcons.vue';
+
 export default {
   name: 'Footer',
-  data() {
-    return {
-      icons: [
-        {
-          to: 'https://buymecoffee.com',
-          icon: 'mdi-coffee',
-        },
-        {
-          to: 'https://github.com/cahuizar/priconne-helper',
-          icon: 'mdi-github',
-        },
-      ],
-    };
+  components: {
+    PNavIcons: NavIcons,
   },
 };
 </script>
