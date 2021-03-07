d7fabc74214292279412a2dbbfe61beb6b6a67fc
Carlos Huizar <hire@carloshuizar.com>
2021-03-06 20:20:07 -0500

    Add vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,21 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/es5/util/colors';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.blue.base,
+        secondary: colors.cyan.base,
+        accent: colors.indigo.base,
+        error: colors.red.base,
+        warning: colors.deepOrange.base,
+        info: colors.yellow.base,
+        success: colors.green.base,
+      },
+    },
+  },
 });
